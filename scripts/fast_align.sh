2574dcd2d79cbec9707780ec230b7ad1343634b1
thomasZen <thomas.zenkel@gmail.com>
2019-01-25 12:17:17 +0000

    Make fastalign and giza install directories external variables

--- scripts/fast_align.sh
+++ scripts/fast_align.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-set -ex
+set -e
+
+# check if MGIZA_DIR is set and installed
+if [ -z ${FASTALIGN_DIR} ]; then
+  echo "Set the variable FASTALIGN_DIR"
+  exit
+fi
+
+if [ ! -f ${FASTALIGN_DIR}/build/fast_align ]; then
+  echo "Install fastalign, file ${FASTALIGN_DIR}/build/fast_align not found"
+  exit
+fi
 
 # check parameter count and write usage instruction
 if (( $# != 2 )); then
@@ -14,14 +25,14 @@ source_name=${1##*/}
 target_name=${2##*/}
 
 # create format used for fastalign
-paste -d "~" ${source_path} ${target_path} | sed 's/~/ ||| /g' | tr '[:upper:]' '[:lower:]' > ${source_name}_${target_name}
-paste -d "~" ${target_path} ${source_path} | sed 's/~/ ||| /g' | tr '[:upper:]' '[:lower:]' > ${target_name}_${source_name}
+paste -d "~" ${source_path} ${target_path} | sed 's/~/ ||| /g' > ${source_name}_${target_name}
+paste -d "~" ${target_path} ${source_path} | sed 's/~/ ||| /g' > ${target_name}_${source_name}
 
 # remove lines which have an empty source or target
 sed -e '/^ |||/d' -e '/||| $/d' ${source_name}_${target_name} > ${source_name}_${target_name}.clean
 sed -e '/^ |||/d' -e '/||| $/d' ${target_name}_${source_name} > ${target_name}_${source_name}.clean
 
 # align in both directions
-/home/thomaszenkel/GitRepos/fast_align/build/fast_align -i ${source_name}_${target_name}.clean -d -o -v > alignment.talp
-/home/thomaszenkel/GitRepos/fast_align/build/fast_align -i ${target_name}_${source_name}.clean -d -o -v > alignment.reverse.talp
+${FASTALIGN_DIR}/build/fast_align -i ${source_name}_${target_name}.clean -d -o -v > alignment.talp
+${FASTALIGN_DIR}/build/fast_align -i ${target_name}_${source_name}.clean -d -o -v > alignment.reverse.talp
 
--- scripts/giza.sh
+++ scripts/giza.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-set -ex
+set -e
+
+SCRIPT_DIR=${0%/giza.sh}
+
+# check if MGIZA_DIR is set and installed
+if [ -z ${MGIZA_DIR} ]; then
+  echo "Set the variable MGIZA_DIR"
+  exit
+fi
+
+if [ ! -f ${MGIZA_DIR}/mgizapp/bin/mgiza ]; then
+  echo "Install mgiza, file ${MGIZA_DIR}/mgizapp/bin/mgiza not found"
+  exit
+fi
 
 # check parameter count and write usage instruction
 if (( $# != 2 )); then
@@ -14,13 +27,13 @@ source_name=${1##*/}
 target_name=${2##*/}
 
 # creates vcb and snt files
-/home/thomaszenkel/GitRepos/mgiza/mgizapp/bin/plain2snt $1 $2
+${MGIZA_DIR}/mgizapp/bin/plain2snt $1 $2
 
-/home/thomaszenkel/GitRepos/mgiza/mgizapp/bin/mkcls -n10 -p${source_path} -V${source_name}.class &
-/home/thomaszenkel/GitRepos/mgiza/mgizapp/bin/mkcls -n10 -p${target_path} -V${target_name}.class &
+${MGIZA_DIR}/mgizapp/bin/mkcls -n10 -p${source_path} -V${source_name}.class &
+${MGIZA_DIR}/mgizapp/bin/mkcls -n10 -p${target_path} -V${target_name}.class &
 
-/home/thomaszenkel/GitRepos/mgiza/mgizapp/bin/snt2cooc ${source_name}_${target_name}.cooc ${source_path}.vcb ${target_path}.vcb ${source_path}_${target_name}.snt &
-/home/thomaszenkel/GitRepos/mgiza/mgizapp/bin/snt2cooc ${target_name}_${source_name}.cooc ${target_path}.vcb ${target_path}.vcb ${target_path}_${source_name}.snt &
+${MGIZA_DIR}/mgizapp/bin/snt2cooc ${source_name}_${target_name}.cooc ${source_path}.vcb ${target_path}.vcb ${source_path}_${target_name}.snt &
+${MGIZA_DIR}/mgizapp/bin/snt2cooc ${target_name}_${source_name}.cooc ${target_path}.vcb ${target_path}.vcb ${target_path}_${source_name}.snt &
 
 
 mkdir -p Forward && cd $_
@@ -44,12 +57,12 @@ cd ..
 
 for name in "Backward" "Forward"; do
   cd $name
-    /home/thomaszenkel/GitRepos/mgiza/mgizapp/bin/mgiza config.txt
+    ${MGIZA_DIR}/mgizapp/bin/mgiza config.txt
     cat *A3.final.part* > allA3.txt
   cd ..
 done
 
 # convert alignments
-/home/thomaszenkel/Scripts/a3ToTalp.py < Forward/allA3.txt > alignment.talp
-/home/thomaszenkel/Scripts/a3ToTalp.py < Backward/allA3.txt > alignment.reverse.talp
+${SCRIPT_DIR}/a3ToTalp.py < Forward/allA3.txt > alignment.talp
+${SCRIPT_DIR}/a3ToTalp.py < Backward/allA3.txt > alignment.reverse.talp
 
