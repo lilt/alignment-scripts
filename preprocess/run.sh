662fb458a44e1a9f72fd1a1d63eb29d11638dc71
thomasZen <thomas.zenkel@gmail.com>
2019-01-30 07:26:58 +0000

    Only use sentencepiece if installed

--- preprocess/run.sh
+++ preprocess/run.sh
@@ -11,6 +11,9 @@ ${PREPROCESS_DIR}/train.sh
 for ln_pair in "roen" "enfr" "deen"; do
   for suffix in "src" "tgt"; do
     cat train/${ln_pair}.lc.${suffix} test/${ln_pair}.lc.${suffix} > train/${ln_pair}.lc.plustest.${suffix}
+
+    # only use sentencepiece if it is installed
+    if command -v spm_train; then
       spm_train --input_sentence_size      100000000 \
                 --model_prefix             train/bpe.${ln_pair}.${suffix} \
                 --model_type               bpe \
@@ -23,9 +26,11 @@ for ln_pair in "roen" "enfr" "deen"; do
                 --vocab_size               10000 \
                 --character_coverage       1.0 \
                 --input                    train/${ln_pair}.lc.plustest.${suffix}
-    spm_encode --model train/bpe.${ln_pair}.${suffix}.model < train/${ln_pair}.lc.${suffix} > train/${ln_pair}.lc.${suffix}.bpe
-    spm_encode --model train/bpe.${ln_pair}.${suffix}.model < train/${ln_pair}.lc.plustest.${suffix} > train/${ln_pair}.lc.plustest.${suffix}.bpe
-    spm_encode --model train/bpe.${ln_pair}.${suffix}.model < test/${ln_pair}.lc.${suffix} > test/${ln_pair}.lc.${suffix}.bpe
+      spm_encode --model train/bpe.${ln_pair}.${suffix}.model < train/${ln_pair}.lc.${suffix} > train/${ln_pair}.lc.${suffix}.bpe
+      spm_encode --model train/bpe.${ln_pair}.${suffix}.model < train/${ln_pair}.lc.plustest.${suffix} > train/${ln_pair}.lc.plustest.${suffix}.bpe
+      spm_encode --model train/bpe.${ln_pair}.${suffix}.model < test/${ln_pair}.lc.${suffix} > test/${ln_pair}.lc.${suffix}.bpe
+    fi
+
   done
 done
  
