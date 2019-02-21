cbe623a2cee71631a6b50830bf2fb473f09860b9
Thomas Zenkel <thomas@lilt.com>
2019-02-21 10:19:08 +0100

    Add script to convert alignments between sentencepiece tokens to alignments between words

---
+++ scripts/sentencepiece_to_word_alignments.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import sys
+import itertools
+import fileinput
+
+def get_mapping(file_path):
+    with open(file_path, "r") as f:
+        for l in f:
+            subwords = l.strip().split()  
+            yield list(itertools.accumulate([int('▁' in x) for x in subwords]))
+
+def convert(src_file, tgt_file):
+    examples = zip(get_mapping(src_file), get_mapping(tgt_file), fileinput.input(files=["-"]))
+    for src_map, tgt_map, line in examples:
+        subword_alignments = {(int(a), int(b)) for a, b in (x.split("-") for x in line.split())}
+        yield {"{}-{}".format(src_map[a], tgt_map[b]) for a, b in subword_alignments}
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Two parameters are required, e.g.: {} text.spm.source text.spm.target < sentence_piece.talp > word.talp".format(sys.argv[0]))
+        exit(1)
+    for word_alignment in convert(*sys.argv[1:]):
+        print(" ".join(word_alignment))
+
