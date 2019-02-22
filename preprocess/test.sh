c2ac952b6a0342229486792409756a6fcf4c301b
thomasZen <thomas.zenkel@gmail.com>
2019-02-22 16:39:07 +0000

    Optionally detokenize the test sets

--- preprocess/test.sh
+++ preprocess/test.sh
@@ -9,6 +9,9 @@ for ln_pair in "roen" "enfr" "deen"; do
   ../preprocess/${ln_pair}-test.sh
   for suffix in "src" "tgt"; do
     ../scripts/lowercase.py < ${ln_pair}.${suffix} > ${ln_pair}.lc.${suffix}
+    if [ -f ${MOSES_DIR}/scripts/tokenizer/detokenizer.perl ]; then
+      ${MOSES_DIR}/scripts/tokenizer/detokenizer.perl < ${ln_pair}.lc.${suffix} > ${ln_pair}.lc.detok.${suffix}
+    fi
   done
 done
 
