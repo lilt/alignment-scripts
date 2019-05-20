bd3c3ba8836008db769226add0bc480123ff5de2
Thomas Zenkel <thomas@lilt.com>
2019-05-20 22:26:05 +0200

    F-measure, option to treat all reference alignments as sure - or to ignore all possible alignments, source and target word coverage, 'internal and external jumps'

--- scripts/aer.py
+++ scripts/aer.py
@@ -15,6 +15,9 @@ def parse_args():
 
     parser.add_argument("--oneRef", help="reference indices start at index 1", action='store_true')
     parser.add_argument("--oneHyp", help="hypothesis indices start at index 1", action='store_true')
+    parser.add_argument("--allSure", help="treat all alignments in the reference as sure alignments", action='store_true')
+    parser.add_argument("--ignorePossible", help="Ignore all possible links", action='store_true')
+    parser.add_argument("--fAlpha", help="alpha parameter used to calculate f measure (has to be set to a value >= 0.0 to report the f-measure)", default=-1.0, type=float)
 
     parser.add_argument("--source", default="", help="the source sentence, used for an error analysis")
     parser.add_argument("--target", default="", help="the target sentence, used for an error analysis")
@@ -22,8 +25,55 @@ def parse_args():
 
     return parser.parse_args()
 
+def calculate_internal_jumps(alignments):
+    """ Count number of times the set of source word indices aligned to a target word index are not adjacent
+        Each non adjacent set of source word indices counts only once 
+    >>> calculate_internal_jumps([{1,2,4}, {42}])
+    1
+    >>> calculate_internal_jumps([{1,2,3,4}])
+    0
+    >>> calculate_internal_jumps([set()])
+    0
+    """
+    jumps = 0
+    for al in alignments:
+        for src in sorted(al)[1:]:
+            if src+1 in al or src-1 in al:
+                pass
+            else:
+                jumps += 1 
+                break
+    return jumps
 
-def calculate_metrics(array_sure, array_possible, array_hypothesis, source_sentences=(), target_sentences=()):
+def calculate_external_jumps(alignments):
+    """ Count number of times the (smallest) source index aligned to target word x is not adjacent or identical to any source word index aligned to the next target word index x+1
+        Target words which do not have any source word aligned to it are ignored
+    >>> calculate_external_jumps([set(), {1,2,4}, {2}, {4}, set()])
+    1
+    """
+
+    jumps = 0
+
+    for prev, current in zip(alignments, alignments[1:]):
+        if len(prev) > 0 and len(current) > 0:
+            src = sorted(prev)[0]
+            if src in current or src+1 in current or src-1 in current:
+                pass
+            else:
+                jumps += 1
+    return jumps
+
+def to_list(A):
+    """ converts set of src-tgt alignments to a list containing a set of aligned source word for each target position
+    >>> to_list({(2,1)})
+    [set(), {2}]
+    """
+    lst = [set() for _ in range(max({y for x,y in A}) + 1)]
+    for x, y in A:
+        lst[y].add(x)
+    return lst
+
+def calculate_metrics(array_sure, array_possible, array_hypothesis, f_alpha, source_sentences=(), target_sentences=()):
     """ Calculates precision, recall and alignment error rate as described in "A Systematic Comparison of Various
         Statistical Alignment Models" (https://www.aclweb.org/anthology/J/J03/J03-1002.pdf) in chapter 5
 
@@ -40,23 +90,40 @@ def calculate_metrics(array_sure, array_possible, array_hypothesis, source_sente
 
     errors = Counter()
 
-    sum_a_intersect_p, sum_a_intersect_s, sum_s, sum_a = 0.0, 0.0, 0.0, 0.0
+    sum_a_intersect_p, sum_a_intersect_s, sum_s, sum_a, aligned_source_words, aligned_target_words = 6 * [0.0]
+    sum_source_words, sum_target_words = map(lambda s: max(1.0, sum(len(x) for x in s)), [source_sentences, target_sentences])
+    internal_jumps, external_jumps = 0, 0
+
     for S, P, A, source, target in itertools.zip_longest(array_sure, array_possible, array_hypothesis, source_sentences, target_sentences):
         sum_a += len(A)
         sum_s += len(S)
         sum_a_intersect_p += len(A.intersection(P))
         sum_a_intersect_s += len(A.intersection(S))
+        aligned_source_words += len({x for x,y in A})
+        aligned_target_words += len({y for x,y in A})
+        al = to_list(A)
+        internal_jumps += calculate_internal_jumps(al)
+        external_jumps += calculate_external_jumps(al)
+
         for src_pos, tgt_pos in A:
             if source and target and (src_pos, tgt_pos) not in P:
-                if source[src_pos] == "," and target[tgt_pos] == "," and False:
-                    print(source, target)
                 errors[source[src_pos], target[tgt_pos]] += 1
 
     precision = sum_a_intersect_p / sum_a
     recall = sum_a_intersect_s / sum_s
     aer = 1.0 - ((sum_a_intersect_p + sum_a_intersect_s) / (sum_a + sum_s))
     
-    return precision, recall, aer, errors
+    if f_alpha < 0.0:
+        f_measure = 0.0
+    else:
+        f_divident = f_alpha / precision
+        f_divident += (1.0 - f_alpha) / recall
+        f_measure = 1.0 / f_divident
+    
+    source_coverage = aligned_source_words / sum_source_words
+    target_coverage = aligned_target_words / sum_target_words
+    
+    return precision, recall, aer, f_measure, errors, source_coverage, target_coverage, internal_jumps, external_jumps
 
 
 def parse_single_alignment(string, reverse=False, one_indexed=False):
@@ -96,9 +163,10 @@ if __name__ == "__main__":
                 sure_alignment = True if '-' in alignment_string else False
                 alignment_tuple = parse_single_alignment(alignment_string, args.reverseRef, args.oneRef)
 
-                if sure_alignment:
+                if sure_alignment or args.allSure:
                     sure[-1].add(alignment_tuple)
-                possible[-1].add(alignment_tuple)
+                if sure_alignment or not args.ignorePossible:
+                    possible[-1].add(alignment_tuple)
 
     with open(args.hypothesis, 'r') as f:
         for line in f:
@@ -108,11 +176,16 @@ if __name__ == "__main__":
                 alignment_tuple = parse_single_alignment(alignment_string, args.reverseHyp, args.oneHyp)
                 hypothesis[-1].add(alignment_tuple)
 
-    precision, recall, aer, errors = calculate_metrics(sure, possible, hypothesis, source, target)
+    precision, recall, aer, f_measure, errors, source_coverage, target_coverage, internal_jumps, external_jumps = calculate_metrics(sure, possible, hypothesis, args.fAlpha, source, target)
     print("{0}: {1:.1f}% ({2:.1f}%/{3:.1f}%/{4})".format(args.hypothesis,
         aer * 100.0, precision * 100.0, recall * 100.0, sum([len(x) for x in hypothesis])))
+    if args.fAlpha >= 0.0:
+        print(f"F-Measure: {f_measure:.3f}")
+    
 
     if args.source:
         assert args.target and args.most_common_errors > 0, "To output the most common errors, define a source and target file and the number of errors to output"
         print(errors.most_common(args.most_common_errors))
+        print(f"Internal Jumps: {internal_jumps}, External Jumps: {external_jumps}")
+        print("Source Coverage: {:.1f}%, Target Coverage: {:.1f}%".format(source_coverage * 100.0, target_coverage * 100.0))
  
