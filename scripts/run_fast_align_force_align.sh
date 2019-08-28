4e35b6bc206f8644c1f7ca6332c99439e1255e44
Thomas Zenkel <thomas@lilt.com>
2019-08-28 09:08:16 +0000

    Output test alignments to correct file for run_fast_align_force_align.sh

--- scripts/run_fast_align_force_align.sh
+++ scripts/run_fast_align_force_align.sh
@@ -9,9 +9,9 @@ for ln_pair in "roen" "deen" "enfr"; do
   train_src="../train/${ln_pair}.lc.src"
   train_tgt="../train/${ln_pair}.lc.tgt"
   ../scripts/fast_align.sh ${train_src} ${train_tgt} ${ln_pair}
-  ../scripts/create_fast_align_corpus.sh ../test/${ln_pair}.lc.src ../test/${ln_pair}.lc.tgt ${ln_pair}.test.fastalign
+  ../scripts/create_fast_align_corpus.sh ../test/${ln_pair}.lc.src ../test/${ln_pair}.lc.tgt ${ln_pair}.test.txt
   # running forced align by hand for each direction requires parsing the error file of the training pass
-  ${FASTALIGN_DIR}/build/force_align.py ${ln_pair}.model ${ln_pair}.error ${ln_pair}.reverse.model ${ln_pair}.reverse.error < ${ln_pair}.test.fastalign > roen.hyp.merge.talp
+  ${FASTALIGN_DIR}/build/force_align.py ${ln_pair}.model ${ln_pair}.error ${ln_pair}.reverse.model ${ln_pair}.reverse.error < ${ln_pair}.test.txt > ${ln_pair}.test.grow-diag-final.talp
 done
 
 cd -
