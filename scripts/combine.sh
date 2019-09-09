453d179c4aea67f8529bf7cca044fca6556c23a6
Thomas Zenkel <thomas@lilt.com>
2019-09-09 14:22:21 +0000

    Also valuate reverse alignments in scripts/combine.sh

--- scripts/combine.sh
+++ scripts/combine.sh
@@ -29,9 +29,9 @@ for method in "grow-diagonal-final" "grow-diagonal" "intersection" "union"; do
   ${SCRIPT_DIR}/combine_bidirectional_alignments.py test.${alignment_name}  test.${alignment_reverse_name} --method $method $4 > test.${alignment_prefix}.${method}.talp
 done
 
-for file_path in test.${alignment_prefix}*.talp; do
+for file_path in test.${alignment_prefix}*.talp test.${alignment_reverse_name}; do
   reverseRef=""
-  if [[ ${file_path} == *"reverse"* ]]; then
+  if [[ ${file_path} == test.${alignment_reverse_name} ]]; then
     reverseRef="--reverseRef"
   fi
   ${SCRIPT_DIR}/aer.py ${reference_path} ${file_path} --oneRef $reverseRef --fAlpha 0.5
